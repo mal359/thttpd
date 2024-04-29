8217aa471dadc914b6118788343ac38dcf81b820
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-04-28 23:11:25 -0400

    Add FreeBSD's patch-thttpd.c by hand.
    The only pertinent hunks were sendfile routines.

--- thttpd.c
+++ thttpd.c
@@ -1796,12 +1796,45 @@ handle_send( connecttab* c, struct timeval* tvP )
         if ( hc->responselen == 0 )
 	    {
 	    /* No, just write the file. */
+#ifdef USE_SENDFILE
+	off_t sbytes;
+	
+	sz = sendfile(
+	     hc->file_fd, hc->conn_fd, c->next_byte_index,
+	     MIN( c->end_byte_index - c->next_byte_index, max_bytes ),
+	     NULL, &sbytes, 0 );
+	if (sz == -1 && errno == EAGAIN)
+	    sz = sbytes > 0 ? sbytes : -1;
+	else if (sz == 0)
+	    sz = sbytes;
+#else
 	    sz = write(
 	        hc->conn_fd, &(hc->file_address[c->next_byte_index]),
 	        MIN( c->end_byte_index - c->next_byte_index, max_bytes ) );
+#endif
 	    }
         else
 	    {
+#ifdef USE_SENDFILE
+	struct sf_hdtr sf;
+	struct iovec iv;
+	off_t sbytes;
+
+	iv.iov_base = hc->response;
+	iv.iov_len = hc->responselen;
+	sf.headers = &iv;
+	sf.hdr_cnt = 1;
+	sf.trailers = NULL;
+	sf.trl_cnt = 0;
+	sz = sendfile(
+	     hc->file_fd, hc->conn_fd, c->next_byte_index,
+	     MIN( c->end_byte_index - c->next_byte_index, max_bytes ),
+	     &sf, &sbytes, 0 );
+	if (sz == -1 && errno == EAGAIN)
+	    sz = sbytes > 0 ? sbytes : -1;
+	else if (sz == 0)
+	    sz = sbytes;
+#else
 	    /* Yes.  We'll combine headers and file into a single writev(),
 	    ** hoping that this generates a single packet.
 	    */
@@ -1812,6 +1845,7 @@ handle_send( connecttab* c, struct timeval* tvP )
 	    iv[1].iov_base = &(hc->file_address[c->next_byte_index]);
 	    iv[1].iov_len = MIN( c->end_byte_index - c->next_byte_index, max_bytes );
 	    sz = writev( hc->conn_fd, iv, 2 );
+#endif
 	    }
 	}
     else
@@ -1902,7 +1936,11 @@ handle_send( connecttab* c, struct timeval* tvP )
 	**
 	** And ECONNRESET isn't interesting either.
 	*/
-	if ( errno != EPIPE && errno != EINVAL && errno != ECONNRESET )
+	if ( errno != EPIPE && errno != EINVAL && errno != ECONNRESET
+#ifdef USE_SENDFILE
+	&& errno != ENOTCONN
+#endif
+	)
 	    syslog( LOG_ERR, "write - %m sending %.80s", hc->encodedurl );
 	clear_connection( c, tvP );
 	return;
